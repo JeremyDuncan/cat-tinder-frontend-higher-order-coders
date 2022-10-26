cbe2b8fa9cae8cc9c10e5df51d7f273e3fa01214
Jeremy Duncan <jduncan46@my.devry.edu>
2022-10-26 16:02:33 -0400

    added notfound styling

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import notFound from "../assets/404_cat.jpeg"
 
 const NotFound = () => {
 
   return (
     <div>
       <h1>NotFound</h1>
+      <img src={notFound} />
     </div>
   )
 }
